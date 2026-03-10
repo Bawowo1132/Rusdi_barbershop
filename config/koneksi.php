d0ec549386d8ac06067cd491f7be656df9545892
Bawowo1132 <ebud1132@gmail.com>
2026-03-10 21:20:23 +0800

    project rusdi barbershop

--- config/koneksi.php
+++ config/koneksi.php
@@ -7,13 +7,16 @@ Fungsi  : Menghubungkan program dengan database MySQL
 ====================================================
 */
 
+// konfigurasi database
 $host = "localhost";
 $user = "root";
 $password = "";
 $database = "rusdi_barbershop";
 
+// membuat koneksi ke database
 $conn = mysqli_connect($host,$user,$password,$database);
 
+// pengecekan koneksi
 if(!$conn){
     die("Koneksi gagal");
 }
